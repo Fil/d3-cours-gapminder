c99c6eb4fdc218cfaaa9cd1a2eb28162446b7d4f
Fil <fil@rezo.net>
2016-06-23 00:20:47 +0200

    pied et entete

---
+++ theme.js
@@ -0,0 +1,25 @@
+d3.select('body')
+.append('header')
+.html('<h1>Cours de D3.js</h1>');
+
+
+var page = window.location.href.match(/page(\d+)\.html/), pages = '';
+
+console.log(page);
+
+if (page) {
+    page = parseInt(page[1]);
+
+    if (page > 1)
+        pages += '<a href=page'+(page-1)+'.html>&lt;</a> | ';
+    else
+        pages += '  | ';
+
+    if (page < 15)
+       pages += '<a href=page'+(page+1)+'.html>&gt;</a>';
+
+}
+
+d3.select('body')
+.append('footer')
+.html(pages);
